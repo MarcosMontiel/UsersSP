791b1c03f5367d4a90fdc0aa3832d7472eecdae0
Marcos Montiel <marcos-gab14@hotmail.com>
2023-07-27 13:03:36 -0600

    Struct was implemented into User files

--- app/src/main/java/com/marcosmontiel/userssp/presentation/screen/user/UserScreen.kt
+++ app/src/main/java/com/marcosmontiel/userssp/presentation/screen/user/UserScreen.kt
@@ -1,8 +1,31 @@
 package com.marcosmontiel.userssp.presentation.screen.user
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Scaffold
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import com.marcosmontiel.userssp.presentation.screen.user.component.UserContent
 
 @Composable
-fun UserScreen() {
+fun UserScreen(navController: NavHostController) {
+
+    val scaffoldState: ScaffoldState = rememberScaffoldState()
+
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = {},
+        bottomBar = {}
+    ) { paddingValues ->
+
+        UserContent(
+            modifier = Modifier.fillMaxSize(),
+            navController = navController,
+            paddingValues = paddingValues,
+        )
+
+    }
 
 }
--- app/src/main/java/com/marcosmontiel/userssp/presentation/screen/user/component/UserContent.kt
+++ app/src/main/java/com/marcosmontiel/userssp/presentation/screen/user/component/UserContent.kt
@@ -1,8 +1,24 @@
 package com.marcosmontiel.userssp.presentation.screen.user.component
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import com.marcosmontiel.userssp.presentation.screen.user.UserViewModel
 
 @Composable
-fun UserContent() {
+fun UserContent(
+    modifier: Modifier,
+    viewModel: UserViewModel = hiltViewModel(),
+    navController: NavHostController,
+    paddingValues: PaddingValues,
+) {
+
+    Box(modifier = modifier.padding(paddingValues)) {
+
+    }
 
 }
