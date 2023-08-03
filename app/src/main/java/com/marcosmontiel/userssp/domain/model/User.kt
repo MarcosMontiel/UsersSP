6f82413b0ff54408380f3fa65ff751c479fc52c5
Marcos Montiel <marcos-gab14@hotmail.com>
2023-08-03 10:05:42 -0600

    Scaffold was modified into UserScreen.kt

--- app/src/main/java/com/marcosmontiel/userssp/domain/model/User.kt
+++ app/src/main/java/com/marcosmontiel/userssp/domain/model/User.kt
@@ -9,4 +9,25 @@ data class User(
 
     fun getFullName(): String = "$name $lastName"
 
+    companion object {
+
+        fun getUsers(): List<User> = listOf(
+            User(
+                name = "Juan",
+                lastName = "Juarez",
+                url = "https://firebasestorage.googleapis.com/v0/b/games-learning.appspot.com/o/Posts%2F3ba3ac98-6487-4cd5-9dc8-5d00831e498f6845442358286615801.jpg?alt=media&token=f671ccdb-5fac-4bed-baaa-9394d2dddcc7"
+            ),
+            User(name = "Maria", lastName = "Juarez"),
+            User(name = "Martha", lastName = "Juarez"),
+            User(name = "Marlene", lastName = "Juarez"),
+            User(name = "Marlene", lastName = "Juarez"),
+            User(name = "Marlene", lastName = "Juarez"),
+            User(name = "Marlene", lastName = "Juarez"),
+            User(name = "Marlene", lastName = "Juarez"),
+            User(name = "Marlene", lastName = "Juarez"),
+            User(name = "Marlene", lastName = "Juarez"),
+        )
+
+    }
+
 }
--- app/src/main/java/com/marcosmontiel/userssp/presentation/screen/user/UserScreen.kt
+++ app/src/main/java/com/marcosmontiel/userssp/presentation/screen/user/UserScreen.kt
@@ -1,14 +1,18 @@
 package com.marcosmontiel.userssp.presentation.screen.user
 
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Scaffold
-import androidx.compose.material.ScaffoldState
-import androidx.compose.material.rememberScaffoldState
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material.icons.rounded.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.marcosmontiel.userssp.domain.model.User
 import com.marcosmontiel.userssp.presentation.screen.user.component.UserContent
+import com.marcosmontiel.userssp.presentation.ui.theme.Blue500
 
 @Composable
 fun UserScreen(navController: NavHostController) {
@@ -17,26 +21,60 @@ fun UserScreen(navController: NavHostController) {
 
     Scaffold(
         scaffoldState = scaffoldState,
-        topBar = {},
-        bottomBar = {}
-    ) { paddingValues ->
+        topBar = {
 
-        UserContent(
-            modifier = Modifier.fillMaxSize(),
-            navController = navController,
-            paddingValues = paddingValues,
-            users = listOf(
-                User(
-                    name = "Juan",
-                    lastName = "Juarez",
-                    url = "https://firebasestorage.googleapis.com/v0/b/games-learning.appspot.com/o/Posts%2F3ba3ac98-6487-4cd5-9dc8-5d00831e498f6845442358286615801.jpg?alt=media&token=f671ccdb-5fac-4bed-baaa-9394d2dddcc7"
-                ),
-                User(name = "Maria", lastName = "Juarez"),
-                User(name = "Martha", lastName = "Juarez"),
-                User(name = "Marlene", lastName = "Juarez"),
-            ),
-        )
+            TopAppBar(
+                title = { Text(text = "Usuarios") },
+                navigationIcon = {
 
-    }
+                    IconButton(onClick = { }) {
+                        Icon(
+                            imageVector = Icons.Rounded.Menu,
+                            contentDescription = "menu icon",
+                        )
+                    }
+
+                },
+                actions = {
+
+                    IconButton(onClick = { }) {
+                        Icon(
+                            imageVector = Icons.Rounded.Settings,
+                            contentDescription = "settings icon",
+                        )
+                    }
+
+                },
+                backgroundColor = Blue500,
+                elevation = 24.dp,
+            )
+
+        },
+        content = { paddingValues ->
+
+            UserContent(
+                modifier = Modifier.fillMaxSize(),
+                navController = navController,
+                paddingValues = paddingValues,
+                users = User.getUsers(),
+            )
+
+        },
+        bottomBar = {
+
+            BottomAppBar() {
+
+            }
+
+        },
+        floatingActionButton = {
+
+            FloatingActionButton(onClick = { }) {
+                Icon(imageVector = Icons.Rounded.Add, contentDescription = "FAB icon")
+            }
+
+        },
+        isFloatingActionButtonDocked = false,
+    )
 
 }
